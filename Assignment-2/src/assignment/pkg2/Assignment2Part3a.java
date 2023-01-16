21516214c5e0855542a20bafe27460872b93191a
AlecMcBurney <alecmcburney@gmail.com>
2023-01-16 15:19:35 -0500

    Created Assignment 2 Part 3a.

---
+++ Assignment-2/src/assignment/pkg2/Assignment2Part3a.java
@@ -0,0 +1,19 @@
+/**
+ * Mackenzie Alec McBurney
+ * 2023/01/16
+ * Assignment 2 Part 3
+ * 3. Consider the following code snippet (code indentation has been intentionally removed):
+ *      if ( y == 8 )
+ *      if ( x == 5 )
+ *      System.out.println( "@@@@@" );
+ *      else
+ *      System.out.println( "#####" );
+ *      System.out.println( "$$$$$" );
+ *      System.out.println( "&&&&&" );
+ */
+package assignment.pkg2;
+
+public class Assignment2Part3a {
+    
+    
+}
