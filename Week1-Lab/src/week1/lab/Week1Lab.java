30d7b2d9726c3b3d71accff5dbd1172e6702ba64
AlecMcBurney <alecmcburney@gmail.com>
2023-01-16 12:22:03 -0500

    Added week 1 lab.

--- Week1-Lab/src/week1/lab/Week1Lab.java
+++ Week1-Lab/src/week1/lab/Week1Lab.java
@@ -15,6 +15,7 @@ public class Week1Lab {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Something is definitely happening.");
     }
     
 }
