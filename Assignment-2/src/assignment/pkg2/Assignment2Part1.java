9794f4f7b060f303903d1b5972a4ea9bf9ef6105
AlecMcBurney <alecmcburney@gmail.com>
2023-01-16 15:03:22 -0500

    Fixed to suit requirements and added comments.

--- Assignment-2/src/assignment/pkg2/Assignment2Part1.java
+++ Assignment-2/src/assignment/pkg2/Assignment2Part1.java
@@ -1,27 +1,44 @@
+/**
+ * Mackenzie Alec McBurney
+ * 2023/01/16
+ * Assignment 2 part 1
+ * Implement a program that calculates and prints prime numbers less than 10,000 (use the if-else and
+ * while statements).
+ */
 package assignment.pkg2;
 
-/**
- * Mackenzie Alec McBurney 2023/01/16 Assignment 2 part 1
- */
 public class Assignment2Part1 {
 
     public static void main(String[] args) {
-        
-        //Initialize a variable to keep track if a number has multiple divisors.
-        int factor;
-        
-        //Cycle through numbers 1 through 10000
-        for (int i = 1; i <= 10000; i++) {
+
+        // Initialze a variable to track number of factors, starting number and possible factors.
+        int factor, count, divisor;
+
+        //Start the count at 1.
+        count = 1;
+
+        // Cycle through numbers 1 through 10000.
+        while (count <= 10000) {
+
+            // Set factor to zero for each checked number.
             factor = 0;
-            for (int j = i - 1; j > 1; j--) {
-                if (i % j == 0) {
+
+            // A while loop to check for factor other than the number or one.
+            divisor = count - 1;
+            while (divisor > 1) {
+
+                //If there is a factor other than the number and one, count it to factor.
+                if (count % divisor == 0) {
                     factor++;
                 }
+                divisor--;
             }
+
+            // If there are factors other than the number and one, print the prime number.
             if (factor == 0) {
-                System.out.println(i);
+                System.out.println(count);
             }
-            
+            count++;
         }
 
     }
