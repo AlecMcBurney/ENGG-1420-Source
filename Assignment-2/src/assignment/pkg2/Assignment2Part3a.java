04304d6cb52da78d074cc80402b010aa9056f35d
AlecMcBurney <alecmcburney@gmail.com>
2023-01-16 16:47:16 -0500

    Completed Assignment 2 Part 3a and added comments.

--- Assignment-2/src/assignment/pkg2/Assignment2Part3a.java
+++ Assignment-2/src/assignment/pkg2/Assignment2Part3a.java
@@ -1,19 +1,43 @@
-/**
- * Mackenzie Alec McBurney
- * 2023/01/16
- * Assignment 2 Part 3
- * 3. Consider the following code snippet (code indentation has been intentionally removed):
- *      if ( y == 8 )
- *      if ( x == 5 )
- *      System.out.println( "@@@@@" );
- *      else
- *      System.out.println( "#####" );
- *      System.out.println( "$$$$$" );
- *      System.out.println( "&&&&&" );
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package assignment.pkg2;
 
+/**
+ * Mackenzie Alec McBurney
+ * 2023/01/16
+ * Assignment 2 part 3a
+ * Consider the following code snippet (code indentation has been intentionally removed):
+ * if ( y == 8 )
+ * if ( x == 5 )
+ * System.out.println( "@@@@@" );
+ * else
+ * System.out.println( "#####" );
+ * System.out.println( "$$$$$" );
+ * System.out.println( "&&&&&" );
+ * a. Modify the above code so that its output for x = 5 and y = 8 is as follows:
+ * @@@@@
+ * $$$$$
+ * &&&&&
+ */
 public class Assignment2Part3a {
-    
-    
+    public static void main(String[] args) {
+        
+        // Initialization and declaration of variables x and y.
+        int x,y;
+        x = 5;
+        y = 8;
+        
+        //Prints certain characters depending on the combined values of x and y.
+        if(x ==5){
+            if(y == 8){
+            System.out.println( "@@@@@" );
+            }else{
+            System.out.println( "#####" );
+            }
+            System.out.println( "$$$$$" );
+            System.out.println( "&&&&&" );
+        }
+    }
 }
