db10fe32ad3dbcdcb67fec8d9ef69a5272013657
AlecMcBurney <alecmcburney@gmail.com>
2023-01-20 09:24:15 -0500

    Created Assignment 3 project and part 1.

---
+++ Assignment3/src/assignment3/Assignment3Part1.java
@@ -0,0 +1,21 @@
+/**
+ * Mackenzie Alec McBurney
+ * 2023/01/
+ * Assignment
+ */
+package assignment3;
+
+/**
+ *
+ * @author kyure
+ */
+public class Assignment3Part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
