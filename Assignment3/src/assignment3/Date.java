36950c7219ccd3600e01bc77e201cbfa20505cd1
AlecMcBurney <alecmcburney@gmail.com>
2023-01-21 18:06:08 -0500

    Created Date.java and setup the methods.

---
+++ Assignment3/src/assignment3/Date.java
@@ -0,0 +1,35 @@
+/**
+ * Mackenzie Alec McBurney
+ * 2023/01/21
+ * Assignment 3 part
+ * Implement a Date class for the concept of date. Each date object contains year, month and day.
+ * Further, each date object must have the following methods:
+ *  a. print, that prints the value of the date in the output.
+ *  b. addDays, which takes a parameter n of type int and adds n days to the date.
+ *  c. compare, which takes a parameter of type Date and calculates the number of days between the
+ *      date of the current object and the input object.
+ */
+package assignment3;
+
+/**
+ *
+ * @author kyure
+ */
+public class Date {
+
+    void print() {
+
+    }
+
+    int addDays() {
+
+    }
+    
+    Date compare() {
+        
+    }
+    public static void main(String args) {
+
+    }
+
+}
