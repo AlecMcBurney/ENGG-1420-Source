bd7d0047f9b9099367ace93a8a8415974a670643
AlecMcBurney <alecmcburney@gmail.com>
2023-01-22 02:39:10 -0500

    Added conditions to addDays method.

--- Assignment3/src/assignment3/Date.java
+++ Assignment3/src/assignment3/Date.java
@@ -17,19 +17,81 @@ package assignment3;
  */
 public class Date {
 
-    void print() {
+    int year, month, day, date[];
 
+    void print(int date[]) {
+        System.out.println(date[0] + "/" + date[1] + "/" + date[2]);
     }
 
-    int addDays() {
+    int[] addDays(int add, int date[]) {
+        date[2] += add;
 
+        if (date[1] == 1
+                || date[1] == 3
+                || date[1] == 5
+                || date[1] == 7
+                || date[1] == 8
+                || date[1] == 10
+                || date[1] == 12) {
+
+            if (date[2] > 31) {
+                date[1] += 1;
+                while (date[2] > 31) {
+                    date[2] = date[2] - 31;
+                }
+            }
+        } else if (date[1] == 4
+                || date[1] == 6
+                || date[1] == 9
+                || date[1] == 11) {
+
+            if (date[2] > 30) {
+                date[1] += 1;
+                while (date[2] > 30) {
+                    date[2] = date[2] - 30;
+                }
+            }
+
+        } else if (date[1] == 2) {
+            if (date[0] % 4 == 0) {
+
+                if (date[2] > 29) {
+                    date[1] += 1;
+                    while (date[2] > 29) {
+                        date[2] = date[2] - 29;
+                    }
+                }
+            } else {
+                if (date[2] > 28) {
+                    date[1] += 1;
+                    while (date[2] > 28) {
+                        date[2] = date[2] - 28;
+                    }
+                }
+            }
+            return date;
+        } else {
+            System.out.println("Error: that is not a correct month number.");
+        }
+
+        Date compare(){
+            
     }
+}
     
-    Date compare() {
-        
-    }
+
+    
+
     public static void main(String args) {
 
     }
 
 }
+/* Days and months notes
+months with 31 days
+1,3,5,7,8,10,12
+months with 30 days
+4,6,9,11
+month 2 with 28 days
+ */
+//2000/4/30
