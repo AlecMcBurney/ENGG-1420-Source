96694cb354a6f4ab4e7f36f3f4399a425df3cebf
AlecMcBurney <alecmcburney@gmail.com>
2023-01-23 10:00:57 -0500

    Added comments and parameter for the move method.

--- Assignment3/src/assignment3/Program.java
+++ Assignment3/src/assignment3/Program.java
@@ -14,27 +14,35 @@ import java.awt.geom.Arc2D;
  * @author kyure
  */
 public class Program {
+
     public static void main(String[] args) {
-        Rect re = new Rect(); 
+        
+        //Initialize and declare the object re's attributes to preset values.
+        Rect re = new Rect();
         re.length = 13;
         re.width = 4;
         re.x = 0;
         re.y = 0;
+        re.side = new int[]{4, 13};
         
+        // Get the perimeter of the inputted rectangle.
         re.perimeter = re.getPerimeter();
         
+        // Get the area of the inputted rectangle.
         re.area = re.getArea();
         
-        re.chord = re.move();
+        // Move the coordinate of the top left corner of the rectangle.
+        re.chord = re.move(2,4);
         re.x = re.chord[0];
         re.y = re.chord[1];
         
+        // Change the length and width of the rectangle to the value of n.
         re.side = re.changeSize(14);
-        
         re.length = re.side[0];
         re.width = re.side[1];
         
+        // Print of information about the rectangle.
         re.print();
     }
-    
+
 }
