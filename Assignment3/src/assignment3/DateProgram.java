6100822671dbabf9562deb61707ba927148a1a79
AlecMcBurney <alecmcburney@gmail.com>
2023-02-01 13:46:02 -0500

    Done probably.

--- Assignment3/src/assignment3/DateProgram.java
+++ Assignment3/src/assignment3/DateProgram.java
@@ -1,6 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Mackenzie Alec McBurney
+ * 2023/02/01
+ * Assignment 3 part 4
+ * Implement a program to create an object of type Date and use its various methods.
  */
 package assignment3;
 
@@ -9,5 +11,13 @@ package assignment3;
  * @author kyure
  */
 public class DateProgram {
+    public static void main(String[] args) {
+        Date first = new Date(2019, 8, 23);
+        Date second = new Date(2020, 4, 3);
+        first.print();
+        first.addDays(9562);
+        first.print();
+        System.out.println("Two dates are " + first.compare(second) + " days apart.");
+    }
     
 }
