6022cecdb77745d7405b6ec015bc3f866b20b54d
AlecMcBurney <alecmcburney@gmail.com>
2023-02-01 11:01:24 -0500

    Created compare method and finished the first version.

--- Assignment3/src/assignment3/Date.java
+++ Assignment3/src/assignment3/Date.java
@@ -1,6 +1,6 @@
 /**
  * Mackenzie Alec McBurney
- * 2023/01/21
+ * 2023/02/01
  * Assignment 3 part
  * Implement a Date class for the concept of date. Each date object contains year, month and day.
  * Further, each date object must have the following methods:
@@ -17,51 +17,47 @@ package assignment3;
  */
 public class Date {
 
-    private int year, month, day, newYear, newMonth, newDay;
+    private int year, month, day;
+    final private int[] daysPerMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+    /**
+     * Constructor for class Date.
+     *
+     * @param year
+     * @param month
+     * @param day
+     */
     public Date(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
+
     }
 
-    public void addDays(int add, int date[]) {
-        this.day += add;
-        while (this.day > 28) {
+    /**
+     * A method that prints the value of the date in the output.
+     */
+    public void print() {
+        System.out.println("The date is" + this.year + "/" + this.month + "/" + this.day);
+    }
 
-            //Checks if entered the month has 31 days.
-            if (this.month == 1 // January.
-                    || this.month == 3 // March
-                    || this.month == 5 // May
-                    || this.month == 7 // July
-                    || this.month == 8 // August
-                    || this.month == 10 // October
-                    || this.month == 12) { // December
-                if(this.day > 31){
-                    this.month++;
-                    this.day -= 31;
-                } 
-                else break;
-            } // Checks if the entered month has 30 days.
-            else if (this.month == 4
-                    || this.month == 6
-                    || this.month == 9
-                    || this.month == 11) {
-                if(this.day > 30){
-                    this.month++;
-                    this.day -= 30;
-                }
-                else break;
-            } // Checks if the entered month has 28 days.
-            else if (this.month == 2) {
-                if(this.day > 28){
-                    this.month++;
-                    this.day -= 28;
-                }
-                else break;
-            } else {
-                System.out.println("Error: that is not a correct month number.");
+    /**
+     * A method that takes a parameter n of type int and adds n days to the
+     * date.
+     *
+     * @param n
+     * @param date
+     */
+    public void addDays(int n, int date[]) {
+        // Increment the day in a controlled manor.
+        for (int i = 0; i < n; i++) {
+            this.day++;
+            // Check if the days flow over into the next month
+            if (this.day > daysPerMonth[this.month]) {
+                this.day = 1;
+                this.month++;
             }
+            // Check if the months flow over into the next year.
             if (this.month > 12) {
                 year++;
                 this.month = 1;
@@ -69,9 +65,45 @@ public class Date {
         }
     }
 
-    Date compare() {
-        int days1, days2;
+    /**
+     * which takes a parameter of type Date and calculates the number of days
+     * between the date of the current object and the input object.
+     *
+     * @param newDate
+     * @return dayDiff
+     */
+    public int compare(Date newDate) {
+        int days1 = 0;
+        int days2 = 0;
+        int dayDiff = 0;
+        days1 += this.year * 365 + monthsToDays(this.month) + this.day;
+        days2 += newDate.year * 365 + monthsToDays(newDate.month) + newDate.day;
+        dayDiff = Math.abs(days1 - days2);
+        return dayDiff;
+    }
 
+    /**
+     * A method that determines the number of days in a number of months.
+     *
+     * @param month
+     * @return
+     */
+    private int monthsToDays(int month) {
+        int totalDays = 0;
+        for (int i = month; i > 0; i--) {
+            totalDays += daysPerMonth[i];
+        }
+        return totalDays;
+    }
+
+    //Setters and Getters
+    /**
+     * An accessor for the var year
+     *
+     * @return year
+     */
+    public int getYear() {
+        return year;
     }
 
     /**
@@ -84,12 +116,30 @@ public class Date {
     }
 
     /**
+     * An accessor for the var month
+     *
+     * @return month
+     */
+    public int getMonth() {
+        return month;
+    }
+
+    /**
      * A mutator for the var month
      *
      * @param month
      */
     public void setMonth(int month) {
         this.month = month;
+    }
+
+    /**
+     * An accessor for the var day
+     *
+     * @return day
+     */
+    public int getDay() {
+        return day;
     }
 
     /**
@@ -100,34 +150,6 @@ public class Date {
     public void setDay(int day) {
         this.day = day;
     }
-
-    /**
-     * An accessor for the var year
-     *
-     * @return year
-     */
-    public int getYear() {
-        return year;
-    }
-
-    /**
-     * An accessor for the var month
-     *
-     * @return month
-     */
-    public int getMonth() {
-        return month;
-    }
-
-    /**
-     * An accessor for the var month
-     *
-     * @return day
-     */
-    public int getDay() {
-        return day;
-    }
-
 }
 /* Days and months notes
 months with 31 days
