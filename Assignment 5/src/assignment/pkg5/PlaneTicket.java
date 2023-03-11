cbfedbcb2daa60baeed0d1f1e2638c9072a0fe64
AlecMcBurney <alecmcburney@gmail.com>
2023-03-11 18:02:45 -0500

    Created class.

---
+++ Assignment 5/src/assignment/pkg5/PlaneTicket.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment.pkg5;
+
+/**
+ *
+ * @author kyure
+ */
+public class PlaneTicket extends Ticket{
+    
+    public PlaneTicket(String name, String lastName, String natCode, String source, String destination, String depTime, int price) {
+        super(name, lastName, natCode, source, destination, depTime, price);
+    }
+    
+}
