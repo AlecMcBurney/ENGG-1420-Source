9e15142923df291a41e326c706fed0316141c308
AlecMcBurney <alecmcburney@gmail.com>
2023-03-19 22:24:19 -0400

    Created a Person class for the ticket super class

--- Assignment 5/src/assignment/pkg5/Person.java
+++ Assignment 5/src/assignment/pkg5/Person.java
@@ -9,5 +9,42 @@ package assignment.pkg5;
  * @author kyure
  */
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String natCode;
+
+    public Person(String firstName, String lastName, String natCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.natCode = natCode;
+    }
+    
+    
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNatCode() {
+        return natCode;
+    }
+
+    public void setNatCode(String natCode) {
+        this.natCode = natCode;
+    }
+    
+    
     
 }
